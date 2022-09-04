7abdcdb822688ffeb39debb712a2d4b9cae8b5e9
bubblesnet <73856491+bubblesnet@users.noreply.github.com>
2022-09-04 02:41:58 -0400

    hmmmm

--- server/nodetest.py
+++ server/nodetest.py
@@ -3,7 +3,10 @@ import logging
 import time
 import traceback
 import os
+import document_processing
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info("Starting sense-python")
+    dp = document_processing.DocumentGraphProcessing("./tweets.csv")
+
